1fc8426260854fed7d4df603fc43d58d6d58b711
MuazzazA <ayd.muazzez@gmail.com>
2022-08-28 19:10:29 +0300

    düzenlemeler

---
+++ plants/lib/theme/default_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData defaultTheme() {
+  return ThemeData(
+    scaffoldBackgroundColor: const Color(0xffEEEEEE),
+    primaryColor: const Color(0xffEEEEEE),
+    // primarySwatch:  const Color(0xffE8E8E8),
+    //textTheme: GoogleFonts.monne
+  );
+}
