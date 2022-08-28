6bdb1678b02e0dadeddb7abd60dcbcea433d4bdd
MuazzazA <ayd.muazzez@gmail.com>
2022-08-28 23:27:42 +0300

    home screen route eklendi

--- plants/lib/routes.dart
+++ plants/lib/routes.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:plants/screens/home_screen.dart';
 import 'package:plants/screens/welcome_screen.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/": (context) => const WelcomeScreen(),
+  "/home": (context) => const HomeScreen(),
 };
