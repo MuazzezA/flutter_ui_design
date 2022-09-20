e270751313cc9c2bf9526698d8d3aec360701de5
MuazzazA <ayd.muazzez@gmail.com>
2022-09-20 11:31:54 +0300

    router'a profil sayfası eklendi

--- plants/lib/routes.dart
+++ plants/lib/routes.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:plants/screens/home_screen.dart';
-import 'package:plants/screens/welcome_screen.dart';
+
+import 'screens/home_screen.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/profile.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/": (context) => const WelcomeScreen(),
   "/home": (context) => const HomeScreen(),
+  "/profile": (context) => const ProfileScreen(),
 };
